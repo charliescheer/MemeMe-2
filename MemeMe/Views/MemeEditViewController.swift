09cf10e8e2f453a5afd1977f176e337cdd45918d
charliescheer <charlie.scheer@automattic.com>
2019-07-22 14:48:55 -0600

    Added connections from UI elements to the View Controller.  Change the view controller name to be clearer (ViewControler -> MemeEditViewController

---
+++ MemeMe/Views/MemeEditViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Charlie Scheer on 7/22/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import UIKit
+
+class MemeEditViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func cameraButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func albumButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func shareButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
+
