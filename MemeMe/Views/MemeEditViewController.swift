96ab9e16cf35290e41bfe9a3b15516aa23c49caa
charliescheer <charlie.scheer@automattic.com>
2019-07-25 18:57:46 -0600

    changed the text font colors per the grading rubric

--- MemeMe/Views/MemeEditViewController.swift
+++ MemeMe/Views/MemeEditViewController.swift
@@ -51,8 +51,8 @@ class MemeEditViewController: UIViewController, UIImagePickerControllerDelegate,
         paragraphStyle.alignment = .center
         
         let memeTextAttributes: [NSAttributedString.Key : Any] = [
-            NSAttributedString.Key.strokeColor: UIColor.white,
-            NSAttributedString.Key.foregroundColor: UIColor.black,
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
             NSAttributedString.Key.font: UIFont(name: constants.fontStyle, size: 40)!,
             NSAttributedString.Key.strokeWidth: -2,
             NSAttributedString.Key.paragraphStyle: paragraphStyle
