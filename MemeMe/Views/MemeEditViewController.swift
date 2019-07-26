4a5ab4f1d7df0fbc0e9496e501c25a4583331fac
charliescheer <charlie.scheer@automattic.com>
2019-07-25 18:05:45 -0600

    Added keyboard notification observers.  Setup keyboard to move when text fields are entered

--- MemeMe/Views/MemeEditViewController.swift
+++ MemeMe/Views/MemeEditViewController.swift
@@ -27,6 +27,11 @@ class MemeEditViewController: UIViewController, UIImagePickerControllerDelegate,
     
     override func viewDidAppear(_ animated: Bool) {
         setupView()
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
     }
     
     func setupView() {
@@ -55,6 +60,33 @@ class MemeEditViewController: UIViewController, UIImagePickerControllerDelegate,
     
     func setupTextFields(_ textField: UITextField, _ tag : Int) {
         
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        view.frame.origin.y = -getKeyboardHeight(notification)
+        print("keyboard shows")
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        print("inside get keyboard height")
+        return keyboardSize.cgRectValue.height
+        
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        print("subscribed")
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
     }
     
     func allowTextEditingAndSharing(_ bool : Bool) {
