774b230326f98190aa0fead2adc8dfa0c8f1af27
Charlie Scheer <scheer.charlie@gmail.com>
2019-08-11 18:00:19 -0700

    Added swift files for both BrowseTableViewController and BrowseCollectionViewController

---
+++ MemeMe 2/Views/BrowseCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BrowseCollectionViewController.swift
+//  MemeMe 2
+//
+//  Created by Charlie Scheer on 8/11/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import UIKit
+
+class BrowseCollectionViewController: BrowseViewController {
+    
+}
---
+++ MemeMe 2/Views/BrowseTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BrowseTableViewController.swift
+//  MemeMe 2
+//
+//  Created by Charlie Scheer on 8/11/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import UIKit
+
+class BrowseTableViewController: BrowseViewController {
+    
+}
