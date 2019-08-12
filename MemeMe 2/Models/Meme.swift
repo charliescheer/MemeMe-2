5145900429bbb0a3aa8905bcc3f7cd4abdb7227d
Charlie Scheer <scheer.charlie@gmail.com>
2019-08-12 15:09:09 -0700

    Added encode/decode functions to the Meme struct

--- MemeMe 2/Models/Meme.swift
+++ MemeMe 2/Models/Meme.swift
@@ -9,13 +9,50 @@
 import Foundation
 import UIKit
 
-struct Meme {
-    let topText: String
-    let bottomText: String
-    let image: UIImage
-    let meme: UIImage
+struct Meme: Codable {
+    var topText: String
+    var bottomText: String
+    var image: UIImage
+    var meme: UIImage
     
-
+    enum CodingKeys: String, CodingKey {
+        case topText
+        case bottomText
+        case image
+        case meme
+    }
     
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        topText = try values.decode(String.self, forKey: .topText)
+        bottomText = try values.decode(String.self, forKey: .bottomText)
+        
+        let imageData = try values.decode(Data.self, forKey: .image)
+        let memeData = try values.decode(Data.self, forKey: .meme)
+        
+        if let decodedImage = UIImage.init(data: imageData), let decodedMeme = UIImage.init(data: memeData) {
+            image = decodedImage
+            meme = decodedMeme
+        } else {
+            image = UIImage()
+            meme = UIImage()
+        }
+    }
     
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(topText, forKey: .topText)
+        try container.encode(bottomText, forKey: .bottomText)
+        guard let imageData = image.pngData() else {
+            return
+        }
+        
+        guard let memeData = meme.pngData() else {
+            return
+        }
+        
+        try container.encode(imageData, forKey: .image)
+        try container.encode(memeData, forKey: .meme)
+    }
 }
